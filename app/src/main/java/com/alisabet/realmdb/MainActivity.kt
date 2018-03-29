3d6c97d9b3e63748af9bea804406b747d0e76e6c
Ali <mailalisabet@gmail.com>
2018-03-29 11:04:14 +0430

    march 29 / 2018

--- app/src/main/java/com/alisabet/realmdb/MainActivity.kt
+++ app/src/main/java/com/alisabet/realmdb/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,17 +15,21 @@ class MainActivity : AppCompatActivity() {
     private lateinit var data: TextView
     private lateinit var save: Button
     private lateinit var read: Button
+    private lateinit var readById: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initViews()
-        save.setOnClickListener(View.OnClickListener {
+        save.setOnClickListener({
             val model = Model(id_et.text.toString().trim(), name_et.text.toString().trim())
             RealmHelper.writeToDB(model)
         })
-        read.setOnClickListener(View.OnClickListener {
-            data.text = RealmHelper.readAllFromDB()
+        read.setOnClickListener({
+            data.text = RealmHelper.findAllFromDB()
+        })
+        readById.setOnClickListener({
+            Toast.makeText(this, RealmHelper.findByIdFromDB("1001"), Toast.LENGTH_SHORT).show()
         })
     }
 
@@ -34,5 +39,6 @@ class MainActivity : AppCompatActivity() {
         data = findViewById<TextView>(R.id.data)
         save = findViewById<Button>(R.id.save)
         read = findViewById<Button>(R.id.read)
+        readById = findViewById<Button>(R.id.readById)
     }
 }
--- app/src/main/java/com/alisabet/realmdb/RealmHelper.kt
+++ app/src/main/java/com/alisabet/realmdb/RealmHelper.kt
@@ -1,23 +1,32 @@
 package com.alisabet.realmdb
 
+import android.util.Log
 import io.realm.Realm
 
 class RealmHelper {
 
     companion object {
-        private var realm= Realm.getDefaultInstance()
+        private var realm = Realm.getDefaultInstance()
         fun writeToDB(model: Model){
-            realm.executeTransactionAsync(Realm.Transaction {
+            realm.executeTransactionAsync({
                 realm -> realm.createObject(Model::class.java, model.id).name = model.name
+            }, {
+                Log.i("db", "saved")
+            }, {
+                Log.i("db", "not saved because of : " + it.message)
             })
         }
-        fun readAllFromDB() : String{
+        fun findAllFromDB() : String{
             val models = realm.where(Model::class.java).findAll()
             var string : String = ""
             for (model in models) {
                 string+= model.id + " " + model.name + "\n"
             }
             return string
+        }
+        fun findByIdFromDB(id: String) : String? {
+            val model = realm.where(Model::class.java).equalTo("id", id).findFirst()
+            return model!!.name
         }
     }
 }
